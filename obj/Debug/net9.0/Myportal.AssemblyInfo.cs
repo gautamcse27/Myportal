d2b23e2aa52376094cded00aab94adfb703aaec5
gautamcse27 <khushi27gkcse@gmail.com>
2025-05-26 12:45:59 +0530

    updated toady

--- obj/Debug/net9.0/Myportal.AssemblyInfo.cs
+++ obj/Debug/net9.0/Myportal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Myportal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8258318aff69bbaab262c1a86a88e1069417da9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Myportal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Myportal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
